5bdb101fb9fa539b2a8e6bcd93991a19d6367661
yangxiaomei <yangxiaomei@ybm100.com>
2019-10-18 14:38:30 +0800

    添加移动端模板

--- src/common/inquirer-config.js
+++ src/common/inquirer-config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   prompts: [// momander的提示语
     {
       name: 'name',
@@ -27,14 +27,22 @@ module.exports = {
     {
       name: 'template',
       type: 'list',
-      choices: ['simple_module', 'node_interface_module'],
-      default: 'simple_module',
+      choices: ['simple_vue_module', 'vue_admin_module', 'node_interface_module','vue_vant_app'],
+      default: 'simple_vue_module',
       message: '请选择模板'
+    },
+    {
+      name: 'install',
+      type: 'list',
+      choices: ['uninstall_dependence', 'install_dependence'],
+      default: 'uninstall_dependence',
+      message: '是否需要安装依赖'
     }
   ],
   tpl: { // 模板列表
     simple_vue_module: 'github:logmei/simple_vue_project/#_template', // #后面代表分支，不设置默认是master
     vue_admin_module: 'github:PanJiaChen/vue-element-admin/#master', // #后面代表分支，不设置默认是master
-    node_interface_module: 'github:logmei/simple_node_project'
+    node_interface_module: 'github:logmei/simple_node_project',
+    vue_vant_app: 'github:logmei/logmei-vue-vant/#template'
   }
 };
--- src/common/project-build.js
+++ src/common/project-build.js
@@ -1,12 +1,25 @@
-const download = require('./download-repo.js');
-const config = require('./inquirer-config.js');
-// const handlebars = require('handlebars');
-const chalk = require('chalk');
-const ora = require('ora');
+import download from './download-repo.js';
+import config from './inquirer-config.js';
+import fs from 'fs';
+import handlebars from 'handlebars';
+import chalk from 'chalk';
+import { exec } from 'child_process';
+import ora from 'ora';
+import Settings from './update-settings.js'
 
-module.exports = (p) => {
+function updateSettings(name){
+  Settings[name].forEach(v => {
+      const fileName = `${name}${v}`;
+      const content = fs.readFileSync(fileName).toString();
+      const result = handlebars.compile(content)(p);
+      fs.writeFileSync(fileName, result);
+  })
+}
+
+export default (p, spinner) => {
   console.log(chalk.green('开始下载'));
-  const spinner = ora('开始下载模板....').start();
+  spinner.text = '开始下载模板....';
+  spinner.start();
   download(config.tpl[p.template], p.name, {}, err => {
     if (err) {
       spinner.fail();
@@ -14,7 +27,25 @@ module.exports = (p) => {
       console.log(err);
     } else {
       spinner.succeed();
-      console.log('下载成功');
+      console.log(chalk.green('下载成功'));
+      let spinnerInstall = ora('修改配置项....\n').start();
+
+      updateSettings(p.name);
+      // console.log('\r\n');
+      console.log(chalk.green('项目创建完毕'));
+      spinnerInstall.succeed();
+      if (p.install === 'install_dependence') {
+        spinnerInstall = ora('开始安装依赖....').start();
+        exec(`cd ${p.name} && npm install`, (error, stdout, stderr) => {
+          if (error) {
+            console.log(chalk.red('安装依赖失败'));
+            console.log(error);
+            spinnerInstall.fail();
+          }
+          spinnerInstall.succeed();
+          console.log(chalk.green('安装依赖成功'));
+        });
+      }
     }
   });
 };
